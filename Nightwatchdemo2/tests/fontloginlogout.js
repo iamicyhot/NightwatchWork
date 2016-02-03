1fdf1276114d8bf4df75debb01d400a23edad0a6
Aakash Satija <aakash.satija@monotype.com>
2016-02-03 18:43:34 +0530

    03-02 eve
    additional

--- Nightwatchdemo2/tests/fontloginlogout.js
+++ Nightwatchdemo2/tests/fontloginlogout.js
@@ -4,9 +4,8 @@ module.exports = {
     
     client
 		.url("http://howdy.fonts.com/")
-		.waitForElementVisible(".ajax-has-dropdown a[href='/secure/sign-in']", 6000)
+		.waitForElementVisible(".ajax-has-dropdown a[href='/secure/sign-in']",10000)
 		.click(".ajax-has-dropdown a[href='/secure/sign-in']")
-		//.waitForElementVisible("#be6a8461ab8c410b8b76ca666eb239a2 #UserName", 6000)
 		.waitForElementVisible("ul[class='ajax-dropdown signinform'] li[data-action='target'] #member-signin-section #hovered-member-signin-section form[action='https://howdy.fonts.com/other/customer/signin'] #UserName",3000)
 		.setValue("ul[class='ajax-dropdown signinform'] li[data-action='target'] #member-signin-section #hovered-member-signin-section form[action='https://howdy.fonts.com/other/customer/signin'] #UserName", "0@bla.com")
 		.waitForElementVisible("ul[class='ajax-dropdown signinform'] li[data-action='target'] #member-signin-section #hovered-member-signin-section form[action='https://howdy.fonts.com/other/customer/signin'] #Password",3000)
